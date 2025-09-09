4ee5b8bee81b4ccdb484eb0e1623a16d0d2c1988
Spencer Yue <spenceryue@gmail.com>
2025-09-09 13:54:33 -0500

    starting to write choose5.init

---
+++ NOTES.md
@@ -0,0 +1,9 @@
+## September 9
+
+- Spread operator ...
+- =>
+- learn JS
+- SuperFormula for both choose7 and choose5
+- alt+arrow (swap), shift+alt+arrow (duplicate), ctrl+shift+k (delete)
+  - word hop, line hop, match hop
+-
---
+++ script.js
@@ -0,0 +1,69 @@
+const nChooseK = Array.from({ length: 52 + 1 }, () => new Uint32Array(7 + 1));
+const choose5 = new Uint16Array(2_598_960 ?? nChooseK[5][2]);
+const choose7 = new Uint16Array(133_784_560 ?? nChooseK[7][2]);
+const category = new Uint32Array(7462);
+
+nChooseK.init = () => {};
+
+choose5.init = () => {
+  let ranking = 0;
+
+  STRAIGHT_FLUSH: for (const rank of Array(13 - 3).keys()) {
+    for (const suit of Array(4).keys()) {
+      const index = superFormula(
+        suit + 4 * rank,
+        suit + 4 * (rank - 1),
+        suit + 4 * (rank - 2),
+        suit + 4 * (rank - 3),
+        suit + 4 * (rank - 4)
+      );
+      choose5[index] = ranking;
+    }
+
+    ranking++;
+  }
+
+  _4_KIND: for (const rank of Array(13).keys()) {
+    for (const kicker of Array(13).keys()) {
+      if (rank === kicker) {
+        continue;
+      }
+
+      for (const suit of Array(4).keys()) {
+        const index = superFormula(
+          ...(rank < kicker
+            ? [0 + 4 * rank, 1 + 4 * rank, 2 + 4 * rank, 3 + 4 * rank, suit + 4 * kicker]
+            : [suit + 4 * kicker, 0 + 4 * rank, 1 + 4 * rank, 2 + 4 * rank, 3 + 4 * rank])
+        );
+        choose5[index] = ranking;
+      }
+
+      ranking++;
+    }
+  }
+
+  FULL_HOUSE: for (const _3rank of Array(13).keys()) {
+    for (const _2rank of Array(13).keys()) {
+      if (_3rank === _2rank) {
+        continue;
+      }
+
+      for (const _3suits of Array(4 ?? nChooseK[4][3]).keys()) {
+        for (const _2suits of Array(6 ?? nChoosek[4][2]).keys()) {
+          // 0 1 ... 5
+          // 0 -> [S] [H]
+          // 1 -> [S] [C]
+          // 2 -> [S] [D]
+          // const
+        }
+      }
+      choose5[superFormula()];
+    }
+  }
+};
+
+const superFormula = (...cards) => {
+  // TODO
+  // k = 0..cards[0]-1
+  // sum( nChooseK(51 - k, cards.length-1) )
+};
