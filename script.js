85d461ff9614469f3c2eaed4b02de768aeef6bf8
Spencer Yue <spenceryue@gmail.com>
2025-09-17 15:14:17 -0500

    finished choose5.init

--- script.js
+++ script.js
@@ -1,201 +1,408 @@
 const nChooseK = Array.from({ length: 52 + 1 }, () => new Uint32Array(7 + 1));
-const choose5 = new Uint16Array(2_598_960 ?? nChooseK[5][2]);
-const choose7 = new Uint16Array(133_784_560 ?? nChooseK[7][2]);
-const category = new Uint32Array(7462);
+const choose5 = new Uint16Array(2_598_960 ?? nChooseK[52][5]);
+const choose7 = new Uint16Array(133_784_560 ?? nChooseK[52][7]);
+const category = new Uint8Array(7462);
 
-nChooseK.init = () => {};
+nChooseK.init = () => {
+  for (let n = 0; n < nChooseK.length; ++n) {
+    nChooseK[n][0] = 1;
+    for (let k = 1; k < nChooseK[n].length && k <= n; ++k) {
+      nChooseK[n][k] = nChooseK[n - 1][k - 1] + nChooseK[n - 1][k];
+    }
+  }
+};
 
 choose5.init = () => {
   let ranking = 0;
 
-  STRAIGHT_FLUSH: for (const rank of Array(13 - 3).keys()) {
-    for (const suit of Array(4).keys()) {
-      const index = superFormula(
-        suit + 4 * rank,
-        suit + 4 * (rank + 1),
-        suit + 4 * (rank + 2),
-        suit + 4 * (rank + 3),
-        suit + 4 * (rank + 4)  /*Howard: changed rank - 1/2/3/4 to rank + 1/2/3/4, since A has rank index 0, 2 is 12) */
-          //also, need to add if rank + 4 > 12, then use 0, for the 5432A straightflush 
-      );
-      choose5[index] = ranking;
-    }
-
-    ranking++;
-  }
-
-  _4_KIND: for (const rank of Array(13).keys()) {
-    for (const kicker of Array(13).keys()) {
-      if (rank === kicker) {
-        continue;
-      }
-
+  STRAIGHT_FLUSH: {
+    for (const rank of Array(13 - 3).keys()) {
       for (const suit of Array(4).keys()) {
-        const index = superFormula(
-          ...(rank < kicker
-            ? [0 + 4 * rank, 1 + 4 * rank, 2 + 4 * rank, 3 + 4 * rank, suit + 4 * kicker]
-            : [suit + 4 * kicker, 0 + 4 * rank, 1 + 4 * rank, 2 + 4 * rank, 3 + 4 * rank])
-        );
-        choose5[index] = ranking;
+        choose5[
+          rank === 9 // 5432A "Five high"
+            ? choose5.index(
+                suit + 4 * 0,
+                suit + 4 * rank,
+                suit + 4 * (rank + 1),
+                suit + 4 * (rank + 2),
+                suit + 4 * (rank + 3)
+              )
+            : choose5.index(
+                suit + 4 * rank,
+                suit + 4 * (rank + 1),
+                suit + 4 * (rank + 2),
+                suit + 4 * (rank + 3),
+                suit + 4 * (rank + 4)
+              )
+        ] = ranking;
       }
 
-      //what if rank > kicker, like 3333Q?
-
-      ranking++;
+      category[ranking++] = 0;
     }
   }
 
-  FULL_HOUSE: for (const _3rank of Array(13).keys()) {
-    for (const _2rank of Array(13).keys()) {
-      if (_3rank === _2rank) {
-        continue;
-      }
+  _4_KIND: {
+    for (const rank of Array(13).keys()) {
+      for (const kicker of Array(13).keys()) {
+        if (rank === kicker) {
+          continue;
+        }
 
-      for (const _3suits of Array(4 ?? nChooseK[4][3]).keys()) {
-        for (const _2suits of Array(6 ?? nChoosek[4][2]).keys()) {
-          // 0 1 ... 5
-          // 0 -> [S] [H]
-          // 1 -> [S] [C]
-          // 2 -> [S] [D]
-          // const
+        for (const suit of Array(4).keys()) {
+          choose5[
+            rank < kicker
+              ? choose5.index(0 + 4 * rank, 1 + 4 * rank, 2 + 4 * rank, 3 + 4 * rank, suit + 4 * kicker)
+              : choose5.index(suit + 4 * kicker, 0 + 4 * rank, 1 + 4 * rank, 2 + 4 * rank, 3 + 4 * rank)
+          ] = ranking;
+        }
+
+        category[ranking++] = 1;
+      }
+    }
+  }
+
+  FULL_HOUSE: {
+    for (const _3rank of Array(13).keys()) {
+      for (const _2rank of Array(13).keys()) {
+        if (_3rank === _2rank) {
+          continue;
+        }
+
+        for (const _3suits of [
+          [0, 1, 2],
+          [0, 1, 3],
+          [0, 2, 3],
+          [1, 2, 3],
+        ]) {
+          for (const _2suits of [
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [1, 2],
+            [1, 3],
+            [2, 3],
+          ]) {
+            choose5[
+              _3rank < _2rank
+                ? choose5.index(
+                    _3suits[0] + 4 * _3rank,
+                    _3suits[1] + 4 * _3rank,
+                    _3suits[2] + 4 * _3rank,
+                    _2suits[0] + 4 * _2rank,
+                    _2suits[1] + 4 * _2rank
+                  )
+                : choose5.index(
+                    _2suits[0] + 4 * _2rank,
+                    _2suits[1] + 4 * _2rank,
+                    _3suits[0] + 4 * _3rank,
+                    _3suits[1] + 4 * _3rank,
+                    _3suits[2] + 4 * _3rank
+                  )
+            ] = ranking;
+          }
+        }
+
+        category[ranking++] = 2;
+      }
+    }
+  }
+
+  FLUSH: {
+    for (let r0 = 0 + 0; r0 < 13 - 4; ++r0) {
+      for (let r1 = r0 + 1; r1 < 13 - 3; ++r1) {
+        // 5432A "Five high"
+        if (r0 === 0 && r1 === 9) {
+          continue;
+        }
+        for (let r2 = r1 + 1; r2 < 13 - 2; ++r2) {
+          for (let r3 = r2 + 1; r3 < 13 - 1; ++r3) {
+            for (let r4 = r3 + 1; r4 < 13; ++r4) {
+              if (r0 + 1 === r1 && r1 + 1 === r2 && r2 + 1 === r3 && r3 + 1 === r4) {
+                continue;
+              }
+              for (const suit of Array(4).keys()) {
+                choose5[choose5.index(suit + 4 * r0, suit + 4 * r1, suit + 4 * r2, suit + 4 * r3, suit + 4 * r4)] =
+                  ranking;
+              }
+              category[ranking++] = 3;
+            }
+          }
         }
       }
-      choose5[superFormula()];
+    }
+  }
+
+  STRAIGHT: {
+    for (const rank of Array(13 - 3).keys()) {
+      for (const s0 of Array(4).keys()) {
+        for (const s1 of Array(4).keys()) {
+          for (const s2 of Array(4).keys()) {
+            for (const s3 of Array(4).keys()) {
+              for (const s4 of Array(4).keys()) {
+                if (s0 === s1 && s1 === s2 && s2 === s3 && s3 === s4) {
+                  continue;
+                }
+
+                choose5[
+                  rank === 9 // 5432A "Five high"
+                    ? choose5.index(
+                        s0 + 4 * 0,
+                        s1 + 4 * (rank + 0),
+                        s2 + 4 * (rank + 1),
+                        s3 + 4 * (rank + 2),
+                        s4 + 4 * (rank + 3)
+                      )
+                    : choose5.index(
+                        s0 + 4 * (rank + 0),
+                        s1 + 4 * (rank + 1),
+                        s2 + 4 * (rank + 2),
+                        s3 + 4 * (rank + 3),
+                        s4 + 4 * (rank + 4)
+                      )
+                ] = ranking;
+              }
+            }
+          }
+        }
+      }
+      category[ranking++] = 4;
+    }
+  }
+
+  _3_KIND: {
+    for (const _3rank of Array(13).keys()) {
+      for (let k0 = 0; k0 < 13 - 1; ++k0) {
+        for (let k1 = k0 + 1; k1 < 13; ++k1) {
+          if (_3rank === k0 || _3rank === k1) {
+            continue;
+          }
+          for (const _3suits of [
+            [0, 1, 2],
+            [0, 1, 3],
+            [0, 2, 3],
+            [1, 2, 3],
+          ]) {
+            for (const s0 of Array(4).keys()) {
+              for (const s1 of Array(4).keys()) {
+                choose5[
+                  _3rank < k0
+                    ? choose5.index(
+                        _3suits[0] + 4 * _3rank,
+                        _3suits[1] + 4 * _3rank,
+                        _3suits[2] + 4 * _3rank,
+                        s0 + 4 * k0,
+                        s1 + 4 * k1
+                      )
+                    : _3rank > k0 && _3rank < k1
+                    ? choose5.index(
+                        s0 + 4 * k0,
+                        _3suits[0] + 4 * _3rank,
+                        _3suits[1] + 4 * _3rank,
+                        _3suits[2] + 4 * _3rank,
+                        s1 + 4 * k1
+                      )
+                    : choose5.index(
+                        s0 + 4 * k0,
+                        s1 + 4 * k1,
+                        _3suits[0] + 4 * _3rank,
+                        _3suits[1] + 4 * _3rank,
+                        _3suits[2] + 4 * _3rank
+                      )
+                ] = ranking;
+              }
+            }
+          }
+          category[ranking++] = 5;
+        }
+      }
+    }
+  }
+
+  _2_PAIR: {
+    for (let highPair = 0; highPair < 13 - 1; ++highPair) {
+      for (let lowPair = highPair + 1; lowPair < 13; ++lowPair) {
+        for (const kicker of Array(13).keys()) {
+          if (kicker === highPair || kicker === lowPair) {
+            continue;
+          }
+          for (const highSuit of [
+            [0, 1],
+            [0, 2],
+            [0, 3],
+            [1, 2],
+            [1, 3],
+            [2, 3],
+          ]) {
+            for (const lowSuit of [
+              [0, 1],
+              [0, 2],
+              [0, 3],
+              [1, 2],
+              [1, 3],
+              [2, 3],
+            ]) {
+              for (kickerSuit of Array(4).keys()) {
+                choose5[
+                  kicker < highPair
+                    ? choose5.index(
+                        kickerSuit + 4 * kicker,
+                        highSuit[0] + 4 * highPair,
+                        highSuit[1] + 4 * highPair,
+                        lowSuit[0] + 4 * lowPair,
+                        lowSuit[1] + 4 * lowPair
+                      )
+                    : kicker > highPair && kicker < lowPair
+                    ? choose5.index(
+                        highSuit[0] + 4 * highPair,
+                        highSuit[1] + 4 * highPair,
+                        kickerSuit + 4 * kicker,
+                        lowSuit[0] + 4 * lowPair,
+                        lowSuit[1] + 4 * lowPair
+                      )
+                    : choose5.index(
+                        highSuit[0] + 4 * highPair,
+                        highSuit[1] + 4 * highPair,
+                        lowSuit[0] + 4 * lowPair,
+                        lowSuit[1] + 4 * lowPair,
+                        kickerSuit + 4 * kicker
+                      )
+                ] = ranking;
+              }
+            }
+          }
+          category[ranking++] = 6;
+        }
+      }
+    }
+  }
+
+  _1_PAIR: {
+    for (const pair of Array(13).keys()) {
+      for (let r0 = 0; r0 < 13 - 2; ++r0) {
+        for (let r1 = r0 + 1; r1 < 13 - 1; ++r1) {
+          for (let r2 = r1 + 1; r2 < 13; ++r2) {
+            if (pair === r0 || pair === r1 || pair === r2) {
+              continue;
+            }
+            for (const pairSuit of [
+              [0, 1],
+              [0, 2],
+              [0, 3],
+              [1, 2],
+              [1, 3],
+              [2, 3],
+            ]) {
+              for (const s0 of Array(4).keys()) {
+                for (const s1 of Array(4).keys()) {
+                  for (const s2 of Array(4).keys()) {
+                    choose5[
+                      pair < r0
+                        ? choose5.index(
+                            pairSuit[0] + 4 * pair,
+                            pairSuit[1] + 4 * pair,
+                            s0 + 4 * r0,
+                            s1 + 4 * r1,
+                            s2 + 4 * r2
+                          )
+                        : pair > r0 && pair < r1
+                        ? choose5.index(
+                            s0 + 4 * r0,
+                            pairSuit[0] + 4 * pair,
+                            pairSuit[1] + 4 * pair,
+                            s1 + 4 * r1,
+                            s2 + 4 * r2
+                          )
+                        : pair > r1 && pair < r2
+                        ? choose5.index(
+                            s0 + 4 * r0,
+                            s1 + 4 * r1,
+                            pairSuit[0] + 4 * pair,
+                            pairSuit[1] + 4 * pair,
+                            s2 + 4 * r2
+                          )
+                        : choose5.index(
+                            s0 + 4 * r0,
+                            s1 + 4 * r1,
+                            s2 + 4 * r2,
+                            pairSuit[0] + 4 * pair,
+                            pairSuit[1] + 4 * pair
+                          )
+                    ] = ranking;
+                  }
+                }
+              }
+            }
+            category[ranking++] = 7;
+          }
+        }
+      }
+    }
+  }
+
+  HIGH_CARD: {
+    for (let r0 = 0 + 0; r0 < 13 - 4; ++r0) {
+      for (let r1 = r0 + 1; r1 < 13 - 3; ++r1) {
+        // 5432A "Five high"
+        if (r0 === 0 && r1 === 9) {
+          continue;
+        }
+        for (let r2 = r1 + 1; r2 < 13 - 2; ++r2) {
+          for (let r3 = r2 + 1; r3 < 13 - 1; ++r3) {
+            for (let r4 = r3 + 1; r4 < 13; ++r4) {
+              if (r0 + 1 === r1 && r1 + 1 === r2 && r2 + 1 === r3 && r3 + 1 === r4) {
+                continue;
+              }
+              for (const s0 of Array(4).keys()) {
+                for (const s1 of Array(4).keys()) {
+                  for (const s2 of Array(4).keys()) {
+                    for (const s3 of Array(4).keys()) {
+                      for (const s4 of Array(4).keys()) {
+                        if (s0 === s1 && s1 === s2 && s2 === s3 && s3 === s4) {
+                          continue;
+                        }
+                        choose5[choose5.index(s0 + 4 * r0, s1 + 4 * r1, s2 + 4 * r2, s3 + 4 * r3, s4 + 4 * r4)] =
+                          ranking;
+                      }
+                    }
+                  }
+                }
+              }
+              category[ranking++] = 8;
+            }
+          }
+        }
+      }
     }
   }
 };
 
-const superFormula = (...cards) => {
-  // TODO
-  // k = 0..cards[0]-1
-  // sum( nChooseK(51 - k, cards.length-1) )
-};
+choose5.index = (a, b, c, d, e) => nChooseK[a][5] + nChooseK[b][4] + nChooseK[c][3] + nChooseK[d][2] + nChooseK[e][1];
 
+choose7.index = (a, b, c, d, e, f, g) =>
+  nChooseK[a][7] + nChooseK[b][6] + nChooseK[c][5] + nChooseK[d][4] + nChooseK[e][3] + nChooseK[f][2] + nChooseK[g][1];
 
-/**
- * Unrank (index -> cards) for 5-card hands from a 52-card deck.
- * Result is the unique sorted 5-tuple [a,b,c,d,e] in lex order.
- *
- * Theory:
- *  - Combinatorial number system says:
- *      index = C(e,5) + C(d,4) + C(c,3) + C(b,2) + C(a,1)
- *    with 0 <= a<b<c<d<e <= 51 (lex order).
- *  - To unrank, we do a greedy search from the rightmost element:
- *      pick the largest e with C(e,5) <= index, subtract it,
- *      then the largest d<e with C(d,4) <= remaining, subtract it,
- *      ... and so on down to a.
- *
- * Why it works:
- *  - Each term C(x, position) counts how many combinations we skip by
- *    raising the current coordinate x while keeping all earlier coords fixed.
- *  - Greedy never backtracks because binomial coefficients are monotone in x:
- *      if C(x, r) <= R but C(x+1, r) > R, x is the unique maximum valid choice.
- *
- * Numerical safety:
- *  - All binomials here are integers <= C(52,5)=2,598,960, well within JS Number precision.
- *  - We precompute a small Pascal table dp[n][k] (0<=n<=52, 0<=k<=5) for exact integers.
- */
+nChooseK.init();
 
-// ---- 1) Precompute binomial coefficients up to (n=52, k=5) via Pascal's triangle.
-function makeBinomTable(maxN = 52, maxK = 5) {
-  const dp = Array.from({ length: maxN + 1 }, () => Array(maxK + 1).fill(0));
-  for (let n = 0; n <= maxN; n++) {
-    dp[n][0] = 1; // C(n,0) = 1
-    for (let k = 1; k <= Math.min(n, maxK); k++) {
-      // Pascal: C(n,k) = C(n-1,k-1) + C(n-1,k)
-      dp[n][k] = dp[n - 1][k - 1] + dp[n - 1][k];
+choose5.init();
+
+const card = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"].flatMap((rank) =>
+  ["♠️", "♥️", "♦️", "♣️"].map((suit) => rank + suit)
+);
+
+const results = [];
+let i = 0;
+HERE: for (let e = 4; e < 52; ++e) {
+  for (let d = 3; d < e; ++d) {
+    for (let c = 2; c < d; ++c) {
+      for (let b = 1; b < c; ++b) {
+        for (let a = 0; a < b; ++a) {
+          if (results.length === 100) {
+            break HERE;
+          }
+          results.push([[card[a], card[b], card[c], card[d], card[e]].join(" "), choose5[i++]]);
+        }
+      }
     }
   }
-  return (n, k) => (n >= 0 && k >= 0 && k <= maxK && k <= n ? dp[n][k] : 0);
 }
-
-const C = makeBinomTable(52, 5);
-
-// ---- 2) Core: greedy unranking for k=5 from a universe {0..51}.
-function unindex5(index) {
-  // Valid index range: 0 .. C(52,5)-1
-  const TOTAL = C(52, 5); // 2,598,960
-  if (index < 0 || index >= TOTAL) {
-    throw new RangeError(`index out of range (0..${TOTAL - 1}): ${index}`);
-  }
-
-  // We will recover e, d, c, b, a in that order (right to left).
-  // In general: pick largest x with C(x, r) <= remaining, where r = remaining slots (5,4,3,2,1).
-
-  let rem = index;
-
-  // Find e: largest e with C(e,5) <= rem. Note e ∈ [4..51] because C(4,5)=0 and C(5,5)=1.
-  let e = 51;
-  while (C(e, 5) > rem) e--;     // monotone search downward
-  rem -= C(e, 5);
-
-  // Find d: largest d<e with C(d,4) <= rem.
-  let d = e - 1;
-  while (C(d, 4) > rem) d--;
-  rem -= C(d, 4);
-
-  // Find c: largest c<d with C(c,3) <= rem.
-  let c = d - 1;
-  while (C(c, 3) > rem) c--;
-  rem -= C(c, 3);
-
-  // Find b: largest b<c with C(b,2) <= rem.
-  let b = c - 1;
-  while (C(b, 2) > rem) b--;
-  rem -= C(b, 2);
-
-  // Find a: largest a<b with C(a,1) <= rem.
-  let a = b - 1;
-  while (C(a, 1) > rem) a--;
-  rem -= C(a, 1);
-
-  // At this point rem must be 0 if the index was valid.
-  // Sanity checks (optional but nice during development):
-  if (!(0 <= a && a < b && b < c && c < d && d < e && e <= 51)) {
-    throw new Error(`Internal error: non-sorted result [${a},${b},${c},${d},${e}]`);
-  }
-  if (rem !== 0) {
-    throw new Error(`Internal error: leftover remainder ${rem}`);
-  }
-
-  return [a, b, c, d, e];
-}
-
-// ---- 3) (Optional) The forward map for testing: index5([a,b,c,d,e]) = C(a,1)+C(b,2)+C(c,3)+C(d,4)+C(e,5)
-function index5(tuple) {
-  const [a, b, c, d, e] = tuple;
-  if (!(0 <= a && a < b && b < c && c < d && d < e && e <= 51)) {
-    throw new Error(`tuple must be strictly increasing within 0..51: ${tuple}`);
-  }
-  return C(a, 1) + C(b, 2) + C(c, 3) + C(d, 4) + C(e, 5);
-}
-
-// ---- 4) Examples and notes:
-
-// Example A: first (index 0) should be [0,1,2,3,4]
-console.log(unindex5(0)); // [0,1,2,3,4]
-
-// Example B: last (index C(52,5)-1) should be [47,48,49,50,51]
-console.log(unindex5(C(52,5)-1)); // [47,48,49,50,51]
-
-// Example C: roundtrip check on a random index
-const ix = 1_234_567;
-const hand = unindex5(ix);
-const ix2 = index5(hand);
-console.log(ix, hand, ix2); // ix should equal ix2
-
-/**
- * PERFORMANCE NOTES
- * - Each unindex call does at most ~52 down-steps per coordinate in the worst case,
- *   but in practice just a handful because C(x,r) grows quickly.
- * - You can accelerate searches (for very hot loops) using a precomputed monotone
- *   table of thresholds or a binary search over x in [r-1 .. 51] since C(x,r) is monotone.
- *
- * ADAPTATION NOTES
- * - For a general “n choose k”:
- *    unindexK(index, n, k) would pick x_k in [k-1..n-1] s.t. C(x_k,k) <= rem,
- *    then x_{k-1} < x_k s.t. C(x_{k-1}, k-1) <= rem, etc.
- * - For your poker project:
- *    this [a,b,c,d,e] are the **card indices 0..51** (As=0, Ah=1, …, 2c=51 in your scheme).
- *    Once you have these, you can map to (rank,suit) and evaluate hand strength.
- */
+console.log(results);
